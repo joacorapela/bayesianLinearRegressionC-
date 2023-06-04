ce2776f6a442811a289f03f3d4e989f899dd281c
joacorapela <joacorapela@gmail.com>
2023-06-04 21:57:35 +0100

    Added

---
+++ code/scripts/OnlineBayesianLinearRegressionApp/Result.cs
@@ -0,0 +1,14 @@
+using System;
+using MathNet.Numerics.LinearAlgebra;
+
+[Serializable]
+public class Result
+{
+    public double[] x;
+    public double[] y;
+    public double[] t;
+    public double a0;
+    public double a1;
+    public double sigma;
+}
+
