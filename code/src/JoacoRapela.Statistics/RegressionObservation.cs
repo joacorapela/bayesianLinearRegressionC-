c90abddd3362f3fc10c8570227f00ae49703871f
glopesdev <glopesdev@users.noreply.github.com>
2023-08-11 01:19:03 +0100

    Add constructor for input mapping compatibility

--- code/src/JoacoRapela.Statistics/RegressionObservation.cs
+++ code/src/JoacoRapela.Statistics/RegressionObservation.cs
@@ -4,6 +4,12 @@ namespace JoacoRapela.Statistics
 {
     public struct RegressionObservation
     {
+        public RegressionObservation(double x, double t)
+        {
+            this.x = x;
+            this.t = t;
+        }
+
         public double x;
         public double t;
     }
