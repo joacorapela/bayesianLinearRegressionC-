de3418464fa4c98cc5d71f617fbddd4f1930b857
Joaquin Rapela <joacorapela@gmail.com>
2024-07-19 09:27:50 +0100

    Not needed any longer, replaced by a tuple :)

--- code/scripts/BayesianLinearRegressionDemo1/BatchRegressionObsAndPosterior.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-public class BatchRegressionObsAndPosterior
-{
-    public List<RegressionObservation> batchRObs;
-    public PosteriorDataItem pdi;
-}
-
