7c23e6ebc22dfb740a86c624d609a9ceb4d9fd6e
Joaquin Rapela <joacorapela@gmail.com>
2024-09-28 23:04:05 +0100

    Only doing visualization (and not predictions, as before)

--- code/scripts/BayesianLinearRegressionDemo2/PredictionsVsResponsesVis.cs
+++ code/scripts/BayesianLinearRegressionDemo2/PredictionsVsResponsesVis.cs
@@ -10,35 +10,26 @@ using System.Drawing;
 using System.Linq;
 
 
-public class PredictionsVsResponsesVis : IObserver<(IList<RegressionObservation> batchRObs, PosteriorDataItem pdi)>
+public class PredictionsVsResponsesVis : IObserver<((double[], double[]), double[])>
 {
     public AvaPlot avaPlot1;
     public double beta;
 
-    public void OnNext((IList<RegressionObservation> batchRObs, PosteriorDataItem pdi) batchROandPos)
+    public void OnNext(((double[], double[]), double[])  predictionsAndResponses)
     {
         Console.WriteLine("PredictionsVsResponsesVis::OnNext called");
-        List<RegressionObservation> batchRObs = (System.Collections.Generic.List<RegressionObservation>) batchROandPos.batchRObs;
-        PosteriorDataItem pdi = batchROandPos.pdi;
+        (double[], double[]) predictions = predictionsAndResponses.Item1;
+        double[] responses = predictionsAndResponses.Item2;
 
-        // computer predictions
-        double[] t = new double[batchRObs.Count];
-        double[] mean = new double[batchRObs.Count];
-        double[] variance  = new double[batchRObs.Count];
-        for (int i=0; i<batchRObs.Count; i++)
-        {
-            (mean[i], variance[i]) = BayesianLinearRegression.Predict(phi: batchRObs[i].phi, mn: pdi.mn, Sn: pdi.Sn, beta: this.beta);
-            t[i] = batchRObs[i].t;
-        }
-        // var ci95Width = variance.Select(x=>1.96*Math.Sqrt(x)).ToArray();
+        double[] means = predictions.Item1;
 
         // plot means and 95% ci for xDense
         avaPlot1.Plot.Clear();
-        avaPlot1.Plot.AddScatter(t, mean, Color.Blue, lineWidth: 0);
+        avaPlot1.Plot.AddScatter(responses, means, Color.Blue, lineWidth: 0);
         // avaPlot1.Plot.AddFillError(t, mean, ci95Width, Color.FromArgb(50, Color.Blue));
 
         // plot data
-        avaPlot1.Plot.AddScatter(t, t, Color.Red, markerSize: 0);
+        avaPlot1.Plot.AddScatter(responses, responses, Color.Red, markerSize: 0);
         avaPlot1.Plot.YLabel("Predictions");
         avaPlot1.Plot.XLabel("Observations");
 
