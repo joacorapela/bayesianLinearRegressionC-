1cdab903b2ba39976e66483449168324e2d36881
Joaquin Rapela <joacorapela@gmail.com>
2024-10-03 21:18:24 +0100

    Cleaned debug messages

--- code/scripts/BayesianLinearRegressionDemo2/PredictionsCalculator.cs
+++ code/scripts/BayesianLinearRegressionDemo2/PredictionsCalculator.cs
@@ -6,18 +6,10 @@ public class PredictionsCalculator
 {
     public double beta;
 
-    // public IObservable<(double, double)> Process(IObservable<(Vector<double>, PosteriorDataItem)> source)
-    public IObservable<(double, double, double)> Process(IObservable<(RegressionObservation, PosteriorDataItem)> source)
-     {
+    public IObservable<(double, double)> Process(IObservable<(Vector<double>, PosteriorDataItem)> source)
+    {
         Console.WriteLine("PredictionsCalculator::Process called");
-        // IObservable<(double, double)> answer = source.Select(phiAndPDI => BayesianLinearRegression.Predict(phi: phiAndPDI.Item1, mn: phiAndPDI.Item2.mn, Sn: phiAndPDI.Item2.Sn, beta: this.beta));
-        IObservable<(double, double, double)> answer = source.Select(phiAndPDI => 
-        {
-            var prediction = BayesianLinearRegression.Predict(phi: phiAndPDI.Item1.phi, mn: phiAndPDI.Item2.mn, Sn: phiAndPDI.Item2.Sn, beta: this.beta);
-            Console.WriteLine($"prediction.mean={prediction.Item1}, observation={phiAndPDI.Item1.t}");
-            var answer2 = (prediction.Item1, prediction.Item2, phiAndPDI.Item1.t);
-            return answer2;
-        });
+        IObservable<(double, double)> answer = source.Select(phiAndPDI => BayesianLinearRegression.Predict(phi: phiAndPDI.Item1, mn: phiAndPDI.Item2.mn, Sn: phiAndPDI.Item2.Sn, beta: this.beta));
         return answer;
      }
 }
